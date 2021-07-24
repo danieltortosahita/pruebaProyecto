6be814e9dc7e6864a3648f9f8e363beeba33102d
Daniel Tortosa <danieltortosahita@gmail.com>
2021-07-24 17:56:32 +0200

    Creación del proyecto.

---
+++ PruebaProyecto24072021/src/main/java/com/cursoaepi/pruebaproyecto24072021/PruebaProyecto24072021.java
@@ -0,0 +1,10 @@
+package com.cursoaepi.pruebaproyecto24072021;
+
+public class PruebaProyecto24072021 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Primera línea de código.");
+    }
+    
+}
